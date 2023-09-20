fdd5da5670924f9aa9f9ccfeb6d576c06de625ef
AnHyeJin <a__hj@naver.com>
2023-09-20 15:14:32 +0900

    커밋 2 뺴기 기능 추가

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 def add_func(n1,n2):
     return n1+n2
 
+def sub_func(n1,n2):
+    return n1-n2
+
 
 
 ## 전역 변수부
@@ -17,5 +20,8 @@ num2 = 200
 
 res = add_func(num1,num2)
 print(num1,'+',num2,'=',res)
+
+res = sub_func(num1,num2)
+print(num1,'-',num2,'=',res)
 
 
